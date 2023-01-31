c61be05426474dfe552114a84158d0b24b951530
Gerard Di Mattia <gerard.dimattia@jjay.cuny.edu>
2023-01-31 17:41:47 -0500

    testing R

---
+++ jan31.r
@@ -0,0 +1,20 @@
+10 + 5
+
+abs(-4.7)
+
+sqrt(16)
+
+numbers <- c(1,2,3) #= also is used <-
+numbers
+
+x<-list("hey","hello","goodbye")
+print(x)
+
+# Enter in the vector of data:
+dat <- c(-2*pi, -1.5*pi, -1*pi, -0.5*pi, 0, 0.5*pi, 1*pi,1.5*pi, 2*pi)
+dat
+
+# seq generates a sequence:
+x <- seq(from = -5 *  pi, to = 2.8 * pi, length.out = 100)
+y <- cos(x)
+plot(x, y, type = "l")
